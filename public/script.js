eaeb53637ad769974b83cd93f220da2901f910d8
Dillon Porter <dillonporter@hotmail.com>
2023-07-12 17:27:00 -0230

    fixed code

--- public/script.js
+++ public/script.js
@@ -1,22 +1,50 @@
-function submitForm(event) {
-    event.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
+const couponCode = require('coupon-code');
+require('dotenv').config();
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', '/send-email', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            window.location.href = '/thankyou.html'; // Redirect to the thank you page
-        } else {
-            alert('Error sending email');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/send-email', (req, res) => {
+    const { name, email } = req.body;
+    const generatedCouponCode = couponCode.generate();
+
+    const transporter = nodemailer.createTransport({
+        service: 'hotmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASSWORD
         }
+    });
+
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: 'New Subscription',
+        text: `A new user subscribed to BarkerBox:\nName: ${name}\nEmail: ${email}\nCoupon Code: ${generatedCouponCode}`
     };
 
-    xhr.send(JSON.stringify({
-        name,
-        email
-    }));
-}
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent:', info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server started on port 3000');
+});
